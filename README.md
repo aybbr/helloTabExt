9b78e44d4b13b198e622df414a460bb43192f760
aybbr <ayoub.briki@gmail.com>
2019-03-11 00:07:47 +0100

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Tableau Extensions run test
 
 This is my first Tableau extension building a simple bar chart using chart.js
+
+This extension :
+- Retrieves Summary Data from a Tableau Workbook.
+- Hook the data to a third party visualization library in this case : chart.js.
+- Render the chart.
--- js/application.js
+++ js/application.js
@@ -1,5 +1,58 @@
 
-$(document).ready(function () {
+	
+'use strict';
+  
+(function () {
+   $(document).ready(function () {
+      // Initializes Tableau extensions Async
+      tableau.extensions.initializeAsync().then(function () {
+      // Once we initialize we call the renderchart function
+      renderChart();
+   }, function () { console.log('Initializing error: ' + err.toString()); });
+   });
+ 
+   // This javascript function gets data from the worksheet and draws the barchart
+   function renderChart() {
+      // Gets all the worksheets in a Tableau Dashboard
+      const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+      console.log(worksheet)
+      // Finds a worksheet called modes
+      var worksheet = worksheets.find(function (sheet) {
+         return sheet.name === "modes";
+      });
+ 
+      // Call a function on the worksheet Object to get the Summary Data.
+      worksheet.getSummaryDataAsync().then(function (sumdata) {
+         // Create an empty arrays for our labels and data set.
+         var labels = [];
+         var data = [];
+          
+         // We get our summary data:
+         var modes = sumdata.data;
+         // We loop through our summary data and hardcode which columns goes into Label
+         // and which column goes into the array.
+         for (var i = 0; i < modes.length; i++) {
+            labels.push(modes[i][0].formattedValue);
+            data.push(modes[i][1].value);
+         }
+ 
+         // Draw the chart as before.
+         var ctx = $("#myChart");
+         var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+               labels: labels, // This now comes from Tableau
+               datasets: [{
+                  backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                  data: data // This now comes from Tableau
+               }]
+            }
+         });
+      });
+   }
+})();
+
+/* $(document).ready(function () {
     var ctx = $("#myChart");
     var myChart = new Chart(ctx, {
         type: 'bar',
@@ -20,4 +73,4 @@ $(document).ready(function () {
           }
         }
     });
-});
+}); */
